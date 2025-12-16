67f912a385abf02f036aef2ff82db66c22593ca1
farid <faridramadhan59@gmail.com>
2025-12-16 17:31:31 +0700

    feat(config): add ClientConfig structure and configuration options

---
+++ clients/config/config.go
@@ -0,0 +1,43 @@
+package clients
+
+import "github.com/parnurzeal/gorequest"
+
+type ClientConfig struct {
+	client       *gorequest.SuperAgent
+	baseURL      string
+	signatureKey string
+}
+
+type IClienConfig interface {
+	Client() *gorequest.SuperAgent
+	BaseURL() string
+	SignatureKey() string
+}
+
+type Option func(*ClientConfig)
+
+func NewClientConfig(options ...Option) IClienConfig {
+	clientConfig := &ClientConfig{
+		client: gorequest.New().Set("Content-Type", "appliaction/json").Set("Accept", "application/json"),
+	}
+	for _, option := range options {
+		option(clientConfig)
+	}
+	return clientConfig
+}
+
+func (c *ClientConfig) Client() *gorequest.SuperAgent { return c.client }
+func (c *ClientConfig) BaseURL() string               { return c.baseURL }
+func (c *ClientConfig) SignatureKey() string          { return c.signatureKey }
+
+func WithBaseURL(baseURL string) Option {
+	return func(c *ClientConfig) {
+		c.baseURL = baseURL
+	}
+}
+
+func WithSignatureKey(signatureKey string) Option {
+	return func(c *ClientConfig) {
+		c.signatureKey = signatureKey
+	}
+}
